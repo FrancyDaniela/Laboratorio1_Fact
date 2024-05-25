65d7a138097197d3764312c9d5e4d4f4ffa74c78
FrancyDaniela <158625837+FrancyDaniela@users.noreply.github.com>
2024-05-25 08:26:56 -0500

    Cambios en la capa vista

---
+++ Vista/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda Articulos</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+    <img class="imagen" src="https://cdn-icons-png.flaticon.com/512/6073/6073873.png"> <br>
+    <form action="validacionUser.php" method="POST">
+        <h1>Bienvenido</h1>
+        <div>
+        <label class="contenidoInput" for="usuario">Usuario:</label>
+        <input class="nombreInput" type="text" id="usuario" name="usuario" placeholder="Ingrese su usuario">
+
+        </div>
+        <div>
+            <label class="contenidoInput" for="password">Contraseña: </label>
+            <input class="nombreInput" type="password" id="password" name="password" placeholder="Ingrese su contraseña">
+        </div>
+        <div>
+            <input class="btnfos botonIngresar" type="submit" value="Ingresar">
+        </div>
+    </form>
+</body>
+</html>
